21643588e1bc45bd58c97cf5ff9a9f9f8d1c824d
Chris Lucas <chris@chrisjlucas.com>
2017-04-02 19:28:24 -0700

    Clean up TheTVDB.User

--- lib/user.ex
+++ lib/user.ex
@@ -1,6 +1,8 @@
 defmodule TheTVDB.User do
   use TheTVDB.Model
 
+  @rating_types [:series, :episode, :image]
+
   model do
     field "favoritesDisplaymode"
     field "language"
@@ -17,91 +19,54 @@ defmodule TheTVDB.User do
     end
   end
 
-  def info, do: do_info()
-  def info(username) do
-    scope(username) |> do_info
-  end
-
-  defp do_info(scope \\ :user) do
-    case TheTVDB.API.get("/user", scope: scope) do
+  def info(username \\ nil) do
+    case TheTVDB.API.get("/user", scope: scope(username)) do
       {:ok, %{"data" => data}} ->
         from_json(data)
     end
   end
-  
-  def favorites, do: do_favorites()
-  def favorites(username) do
-    scope(username) |> do_favorites
-  end
 
-  def do_favorites(scope \\ :user) do
-    case TheTVDB.API.get("/user/favorites", scope: scope) do
+  def favorites(username \\ nil) do
+    case TheTVDB.API.get("/user/favorites", scope: scope(username)) do
       {:ok, %{"data" => data}} ->
         data["favorites"]
     end
   end
-  
-  def add_favorite(series_id), do: do_add_favorite(series_id)
-  def add_favorite(username, series_id) do
-    scope(username) |> do_add_favorite(series_id)
-  end
 
-  defp do_add_favorite(scope \\ :user, series_id) do
+  def add_favorite(username \\ nil, series_id) do
     endpoint = "/user/favorites/#{series_id}"
-    case TheTVDB.API.put(endpoint, "", scope: scope) do
+    case TheTVDB.API.put(endpoint, "", scope: scope(username)) do
       {:ok, _} ->
         :ok
     end
   end
-  
-  def remove_favorite(series_id), do: do_remove_favorite(series_id)
-  def remove_favorite(username, series_id) do
-    scope(username) |> do_remove_favorite(series_id)
-  end
 
-  defp do_remove_favorite(scope \\ :user, series_id) do
+  def remove_favorite(username \\ nil, series_id) do
     endpoint = "/user/favorites/#{series_id}"
-    TheTVDB.API.delete(endpoint, scope: scope)
+    TheTVDB.API.delete(endpoint, scope: scope(username))
   end
 
-  def ratings, do: do_ratings()
-  def ratings(username) do
-    scope(username) |> do_ratings
-  end
-
-  def do_ratings(scope \\ :user) do
-    TheTVDB.API.get_stream("/user/ratings", scope: scope)
+  def ratings(username \\ nil) do
+    TheTVDB.API.get_stream("/user/ratings", scope: scope(username))
     |> Stream.map(&Rating.from_json/1)
   end
 
-  def add_rating(type, item, rating), do: do_add_rating(type, item, rating)
-  def add_rating(username, type, item, rating) do
-    scope(username) |> do_add_rating(type, item, rating)
-  end
-
-  defp do_add_rating(scope \\ :user, type, item, rating)
-      when type in [:series, :episode, :image] and is_integer(rating) do
+  def add_rating(username \\ nil, type, item, rating)
+      when type in @rating_types and is_integer(rating) do
     type = Atom.to_string(type)
     endpoint = "/user/ratings/#{type}/#{item}/#{rating}"
-    case TheTVDB.API.put(endpoint, "", scope: scope) do
+    case TheTVDB.API.put(endpoint, "", scope: scope(username)) do
       {:ok, _} ->
         :ok
     end
   end
-  
-  def remove_rating(type, item), do: do_remove_rating(type, item)
-  def remove_rating(username, type, item) do
-    scope(username) |> do_remove_rating(type, item)
-  end
 
-  defp do_remove_rating(scope \\ :user, type, item)
-      when type in [:series, :episode, :image] do
+  def remove_rating(username \\ nil, type, item) when type in @rating_types do
     type = Atom.to_string(type)
     endpoint = "/user/ratings/#{type}/#{item}"
-    TheTVDB.API.delete(endpoint, scope: scope)
+    TheTVDB.API.delete(endpoint, scope: scope(username))
   end
 
-  defp scope(username) do
-    {:user, username}
-  end
+  defp scope(username) when is_nil(username), do: :user
+  defp scope(username), do: {:user, username}
 end
