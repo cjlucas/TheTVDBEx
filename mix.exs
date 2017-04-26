1b2d3439691017972a7417ef2528aaf215df1f4f
Chris Lucas <chris@chrisjlucas.com>
2017-04-26 14:33:01 -0700

    Bump 1.0.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule TheTVDB.Mixfile do
 
   def project do
     [app: :thetvdb,
-     version: "1.0.0-alpha.5",
+     version: "1.0.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
